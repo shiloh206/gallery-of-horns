7eacbecec03e7702b8ede5a7727ccc674ad69969
Ahmed Mohamed <ahmed.mohamed57@outlook.com>
2023-02-27 17:31:58 -0800

    worked on lab1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import HornedBeast from './HornedBeast';
+import Footer from './Footer';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  render(){
+    
+    return(
+      <>
+      <header>
+         <h1>People of 301</h1>
+         </header>
+         <main>
+         </main>
+         <footer>&copy; Code fellows 007</footer>
+         </>
+    );
+  }
 }
 
-export default App;
+export default App; 
+
+
+// ======================================================
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./Main.";
+
+class Footer extends React.Component {
+  render() {
+
+    return (
+      <footer>&copy; Ahmed Mohamed</footer>
+
+  )
+
+  }
+}
